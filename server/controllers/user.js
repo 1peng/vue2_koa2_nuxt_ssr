21b610b4061aa25695211e5edf486f35254607bc
Yipeng <228331207@qq.com>
2017-09-28 10:27:57 +0800

    first commit

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -4,8 +4,19 @@
 import services from '../../helper/services'
 const router = require('koa-router')()
 router.get('/index', async function (ctx, next) {
-  let advList = await services.cmsAdv.getAdvList()
+  let advList = await services.local.getUserList()
   ctx.body = advList
+})
+
+router.get('/list', async function (ctx, next) {
+  ctx.body = [
+    {
+      name:'yipeng', age: '29'
+    },
+    {
+      name:'yihang', age: '18'
+    }
+  ]
 })
 
 module.exports = router
